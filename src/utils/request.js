65ec4f0fc903fb5858caad50fb0870b694ba6dd9
jht <1751263383@qq.com>
2023-03-03 17:49:53 +0800

    Initial commit

---
+++ src/utils/request.js
@@ -0,0 +1,85 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+
+// 创建axios实例
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // 超时设置
+})
+
+// 请求拦截器
+service.interceptors.request.use(
+  config => {
+    // 请求发送之前
+
+    if (store.getters.token) {
+      // 让每个请求携带token
+      // ['Token'] 是自定义标题键
+      // 可以根据实际情况修改
+      config.headers['Token'] = getToken()
+    }
+    return config
+  },
+  error => {
+    // 发送错误时
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+service.interceptors.response.use(
+  /**
+   * 如果您想获取http信息，如头或状态
+   * 请返回response => response
+  */
+
+  /**
+   * 通过自定义代码确定请求状态
+   * 这里只是一个例子
+   * 您也可以通过HTTP状态码来判断状态
+   */
+  response => {
+    const res = response.data
+
+    // 如果自定义代码不是20000，则判定为错误。
+    if (res.code !== 20000) {
+      Message({
+        message: res.message || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+
+      // 50008:非法令牌;50012:已登录的其他客户端;50014:令牌过期;
+      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // 重新登录
+        MessageBox.confirm('您已登出，您可以取消以留在此页面，或重新登录', '确定退出？', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(new Error(res.message || 'Error'))
+    } else {
+      return res
+    }
+  },
+  error => {
+    console.log('err' + error) // 用于debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
